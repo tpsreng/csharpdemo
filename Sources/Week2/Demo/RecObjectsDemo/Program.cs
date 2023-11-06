7e2e8ff39ae79c6744074f42d040d5b8881cacc4
tpsreng <149149924+tpsreng@users.noreply.github.com>
2023-11-06 22:27:34 +0700

    Add files via upload

---
+++ Sources/Week2/Demo/RecObjectsDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace RecObjectsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countWidth = 6;
+            Rectangle.FieldWidth = 15;
+            Rectangle.DecimalPoint = 5;
+            Rectangle rec = new();
+
+            Console.WriteLine("Rectangles");
+            Console.WriteLine("Input width and length separated by space (eg. 2 5) or empty to stop.");
+            
+            var result = "";
+            var totalArea = 0.0;
+            var count = 0;
+            while (true)
+            {
+                Console.Write($"[{count + 1}]: ");
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line)) break;
+                if (rec.SetData(line) == false) continue;
+                
+                totalArea += rec.GetArea();
+                count++;
+                var subResult =string.Format($"{{0,{countWidth}}} {{1}}", count, rec.GetInfo());
+                if (result != "") result += "\n";
+                result += subResult;
+            }
+            
+            if (count == 0) return;
+            
+            string heading = string.Format($"{{0,{countWidth}}} ", "no") + Rectangle.GetHeading();
+            string bar = new string('-', countWidth + 1) + Rectangle.GetBar('-');
+            string totalFormat = $"Total:{{0:N{Rectangle.DecimalPoint}}}";
+            var totalText = string.Format($"{{0,{bar.Length}}}",string.Format(totalFormat, totalArea));
+
+            Console.WriteLine();
+            Console.WriteLine(heading);
+            Console.WriteLine(bar);
+            Console.WriteLine(result);
+            Console.WriteLine(bar);
+            Console.WriteLine(totalText);
+        }
+    }
+}
---
+++ Sources/Week2/Demo/RecObjectsDemo/Rectangle.cs
@@ -0,0 +1,73 @@
+﻿namespace RecObjectsDemo
+{
+    public class Rectangle
+    {
+        #region static members
+        //static fields
+        public static int FieldWidth = 8;
+        public static int DecimalPoint = 2;
+       
+        //static metnods
+        public static string GetHeading()
+        {
+            string nameText = string.Format($"{{0,{FieldWidth}}}", "width");
+            string genderText = string.Format($"{{0,{FieldWidth}}}", "length");
+            string ageText = string.Format($"{{0, {FieldWidth}}}", "area");
+            return $"{nameText} {genderText} {ageText}";
+        }
+
+        public static string GetBar(char c = '-')
+        {
+            return new string(c, FieldWidth * 3 + 2);
+        }
+        #endregion
+
+        #region instance members
+        //instance fields
+        protected double _width = 0.0;
+        protected double _length = 0.0;
+        protected double _area = 0.0;
+        protected bool _isAreaToUpdate = false;
+
+        //instance constructors
+        public Rectangle() { }
+        public Rectangle(double width, double length)
+        {
+            _width = width;
+            _length = length;
+            _isAreaToUpdate = true;
+        }
+        
+        //instance methods
+        public double GetWidth() { return _width; }
+
+        public double GetLength() { return _length; }
+ 
+        public double GetArea() 
+        {  
+            if (_isAreaToUpdate) _area = _width * _length;
+            return  _area; } 
+       
+        public bool SetData(string data, string separator=" ")
+        {
+            string[] sides = data.Split(separator);
+            if (sides.Length < 2) return false;
+            if (double.TryParse(sides[0], out double wd)==false) return false;
+            if(double.TryParse(sides[1], out double lng)==false) return false;
+            _width = wd;
+            _length = lng;
+            _isAreaToUpdate = true;
+            return true;
+        }
+
+        public string GetInfo()
+        {
+            string format ="{0," + $"{FieldWidth}:N{DecimalPoint}" +"}";
+            string wdInfo = string.Format(format, GetWidth());
+            string lngInfo = string.Format(format, GetLength());
+            string areaInfo = string.Format(format, GetArea());
+            return $"{wdInfo} {lngInfo} {areaInfo}";
+        }
+        #endregion
+    }
+}
