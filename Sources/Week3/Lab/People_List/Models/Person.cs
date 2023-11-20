039d81b8370fa1a416e5dc0ef3a56860e24aa493
tpsreng <taingpengsreng@gmail.com>
2023-11-20 15:55:09 +0700

    W3Lab:Applying List of peole

---
+++ Sources/Week3/Lab/People_List/Models/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace People_OOP.Models
+{
+    public class Person
+    {
+        //static members
+        public static int NameFieldWidth { get; set; } = 30;
+        public static int GenderFieldWidth { get; set; } = 6;
+        public static int AgeFieldWidth { get; set; } = 4;
+        //static metnods
+        public static string GetHeading()
+        {
+            string nameText = string.Format($"{{0,{-NameFieldWidth}}}", "name");
+            string genderText = string.Format($"{{0,{-GenderFieldWidth}}}", "gender");
+            string ageText = string.Format($"{{0, {AgeFieldWidth}}}", "age");
+            return $"{nameText} {genderText} {ageText}";
+        }
+        public static string GetBar(char c='-')
+        {
+            return new string(c, NameFieldWidth + GenderFieldWidth + AgeFieldWidth + 2);
+        }
+
+        //instance fields
+        protected string _name="";
+        protected string _gender="";
+        protected byte _age=0;
+
+        //instance methods
+        public string GetName() => _name;
+        public string GetGender() => _gender;  
+        public string SetGender(string gender)=> _gender = gender;
+        public byte GetAge() => _age; 
+
+        public bool SetData(string data, string delimiter)
+        {
+            string[] arr = data.Split(delimiter);
+            if (arr.Length < 3) return false;
+            string name = arr[0].Trim();
+            string gender = arr[1].Trim();
+            if (byte.TryParse(arr[2], out byte age) == false) return false;
+            _name = name;
+            _gender= gender;
+            _age = age;
+            return true;
+        }
+        public string GetInfo()
+        {
+            string nameInfo = string.Format($"{{0,{-NameFieldWidth}}}", _name);
+            string genderInfo = string.Format($"{{0,{-GenderFieldWidth}}}", _gender);
+            string ageInfo = string.Format($"{{0, {AgeFieldWidth}}}", _age);
+            return $"{nameInfo} {genderInfo} {ageInfo}";
+        } 
+    }
+}
---
+++ Sources/Week3/Lab/People_List/Program.cs
@@ -0,0 +1,78 @@
+﻿using People_OOP.Models;
+using System.Text;
+
+namespace People_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string datafile = "people.txt";
+            List<Person> people = GetPeople(datafile);
+
+            Console.WriteLine("\nOriginal people");
+            ViewPeople(people);
+
+            Pause();
+            List<Person> filterPeople = FilterPeopleOverAge(people, 25);
+            filterPeople.Sort(AgeComparison);
+            Console.WriteLine("\nPeople with age>25 in decending of ages");
+            ViewPeople(filterPeople);
+
+            Pause();
+            people.Sort(NameComparison);
+            Console.WriteLine("\nPeople in ascending of names");
+            ViewPeople(people);
+        }
+        static void Pause()
+        {
+            Console.WriteLine("Any key to continue...");
+            Console.ReadKey();
+        }
+        static List<Person> GetPeople(string file)
+        {
+            TextReader reader = Console.In;
+            Console.SetIn(new StreamReader(file));
+            List<Person> result = new();
+            while (true)
+            {
+                string? data = Console.ReadLine();
+                if (data == null) break;
+                Person p = new Person();
+                if (p.SetData(data, "/") == false) continue;
+                result.Add(p);
+            }
+            Console.SetIn(reader);
+            return result;
+        }
+        static void ViewPeople(List<Person> pers)
+        {
+            if (pers.Count == 0)
+            {
+                Console.WriteLine("No people to be shown out");
+            }
+            Console.WriteLine(Person.GetHeading());
+            Console.WriteLine(Person.GetBar());
+            for (int index = 0; index < pers.Count; index++)
+            {
+                Console.WriteLine(pers[index].GetInfo());
+            }
+            Console.WriteLine(Person.GetBar());
+        }
+        static List<Person> FilterPeopleOverAge(List<Person> pers, int age)
+        {
+            List<Person> result = new();
+            foreach (Person p in pers)
+                if (p.GetAge() > age) { result.Add(p); }
+            return result;
+        }
+        static int AgeComparison(Person x, Person y)
+        {
+            return y.GetAge().CompareTo(x.GetAge());
+        }
+        static int NameComparison(Person x, Person y)
+        {
+            return x.GetName().CompareTo(y.GetName());
+        }
+    }
+}
