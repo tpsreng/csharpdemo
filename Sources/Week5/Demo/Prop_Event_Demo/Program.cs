8ddacdc425ad44e42947c988183398a7167b1a27
tpsreng <taingpengsreng@gmail.com>
2023-11-28 17:17:25 +0700

    Week5-Demonstrating on properties and events

---
+++ Sources/Week5/Demo/Prop_Event_Demo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Markup;
+
+namespace Prop_Event_Demo;
+
+internal class Program
+{
+    static List<Rectangle> ModifiedRecs = new();
+    static void Main(string[] args)
+    {
+        
+        //Creating and initializing a list of rectangles
+        List<Rectangle> recs = new List<Rectangle> 
+        { new(4, 6)
+        , new(3, 7)
+        , new(2, 9)
+        , new(5, 8)
+        , new(6, 7)
+        , new(3, 10)
+        };
+
+        //Subcripting for every rectangle's event DataModified
+        recs.ForEach(r => r.DataModified += (r) => 
+        { 
+            ModifiedRecs.Add(r);
+            Console.WriteLine($"The rectangle, {r.No}, was modified");
+        });
+        
+        //Viewing rectangles
+        ViewRectangles(recs);
+        Pause();
+
+        //Modifying 3 rectangles
+        recs[0].Data = new List<double> { 4, 8 };
+        recs[3].Width = 2;
+        recs[5].Length = 12;
+        Pause();
+
+        //Viewing Rectangles
+        ViewRectangles(recs);
+    }
+    static void Pause()
+    {
+        Console.Write("Press any key continue...");
+        Console.ReadKey();
+        Console.WriteLine();
+        Console.WriteLine();
+        
+    }
+    static void ViewRectangles(List<Rectangle> recs)
+    {
+        ConsoleColor color = Console.BackgroundColor;
+        ConsoleColor modColor = ConsoleColor.DarkRed;
+        Console.WriteLine(Rectangle.GetHeading());
+        string bar = Rectangle.GetBar('-');
+        Console.WriteLine(bar);
+        recs.ForEach((r) =>
+        {
+            Console.BackgroundColor = (ModifiedRecs.Contains(r)) ? modColor : color;
+            Console.WriteLine(r.Info);
+        });
+        Console.BackgroundColor = color;
+        Console.WriteLine(bar);
+        string areaTotal = string.Format("Area Total:{0:N3}", recs.Sum(r => r.Area));
+        Console.WriteLine(string.Format($"{{0,{bar.Length}}}", areaTotal));
+
+        ModifiedRecs.Clear();
+    }
+}
---
+++ Sources/Week5/Demo/Prop_Event_Demo/Rectangle.cs
@@ -0,0 +1,85 @@
+﻿namespace Prop_Event_Demo;
+
+public class Rectangle
+{
+    #region static members
+    private static int Instances = 0;
+    //static RW properties
+    public static int FieldWidth { get; set; } = 8;
+    public static int DecimalPoint { get; set; } = 2;
+
+    //static methods
+    public static string GetHeading()
+    {
+        string noText = string.Format($"{{0,{FieldWidth}}}", "no");
+        string nameText = string.Format($"{{0,{FieldWidth}}}", "width");
+        string genderText = string.Format($"{{0,{FieldWidth}}}", "length");
+        string ageText = string.Format($"{{0, {FieldWidth}}}", "area");
+        return $"{noText} {nameText} {genderText} {ageText}";
+    }
+
+    public static string GetBar(char c = '-')
+    {
+        return new string(c, FieldWidth * 4 + 3);
+    }
+    #endregion
+
+    #region instance members
+    //instance events
+    public RectangleHandler? DataModified = null;
+
+    //instance fields
+    protected int _no = 0;
+    protected double _width = 0.0;
+    protected double _length = 0.0;
+
+    //instance constructors
+    public Rectangle() { _no = ++Instances; }
+    public Rectangle(double width, double length)
+    {
+        _no = ++Instances;
+        _width = width;
+        _length = length;
+    }
+    
+    //Instance RW properties
+    public double Width
+    {
+        get { return _width; }
+        set { _width = value; DataModified?.Invoke(this); }
+    }
+    public double Length
+    {
+        get { return _length; }
+        set { _length = value; DataModified?.Invoke(this); }
+    }
+
+    //Write-only property
+    public List<double> Data
+    {
+        set
+        {
+            if (value.Count < 2) return;
+            _width = value[0];
+            _length = value[1];
+            DataModified?.Invoke(this);
+        }
+    }
+    
+    //Read-only properties
+    public int No { get { return _no; } }
+    public double Area { get { return _width * _length; } }
+    public string Info
+    {
+        get
+        {
+            string format = "{0," + $"{FieldWidth}:N{DecimalPoint}" + "}";
+            string noInfo = string.Format($"{{0,{FieldWidth}}}", _no);
+            string wdInfo = string.Format(format, _width);
+            string lngInfo = string.Format(format, _length);
+            string areaInfo = string.Format(format, Area);
+            return $"{noInfo} {wdInfo} {lngInfo} {areaInfo}";
+        }
+    }
+    #endregion
+}
